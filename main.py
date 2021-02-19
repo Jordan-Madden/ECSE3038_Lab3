b8e7f1ae831763560952a15ac461d455c9a7e440
Jordan-Madden <jordanmadden285@gmail.com>
2021-02-19 12:34:52 -0500

    Made a change

--- main.py
+++ main.py
@@ -13,7 +13,6 @@ from json import loads
 
 app = Flask(__name__)
 CORS(app)
-app.config["MONGO_URI"] = "mongodb+srv://neddamj:Ja.sparky77@cluster0.weykq.mongodb.net/monday?retryWrites=true&w=majority"
 mongo = PyMongo(app)
 
 # Super fancy database
